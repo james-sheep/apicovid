a5a6f1f7dae1e414a27fb4eaa63c1acaad127bdf
Carneiro <carneiro@carneiro.ubuntu>
2021-05-04 11:43:01 -0300

    make it

--- principal.py
+++ principal.py
@@ -17,4 +17,7 @@ def index():
     
         return render_template('index.html')
 
-app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    
